3ba08d55b94cc746e6b9d9f260a8c64a195f37ea
Augusto Mendes <augustomendes.mendes9@gmail.com>
2020-02-21 14:25:13 -0300

    Adicionada a fromatação da tabuada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import br.com.icts.utilidades.Util;
+
 public class Program {
 public static void main(String[] args) {
-	System.out.println("Tabuada");
+	System.out.println("                          Tabuada");
+	System.out.println();
 	int[][] mat = new int [10][10];
 	int count = 0;
 	
@@ -11,13 +14,13 @@ public static void main(String[] args) {
 			mat[i][j] = (i +1) * (j +1) ;
 		}
 	}
-	System.out.println("   1    2    3    4    5    6    7    8    9    10");
+	System.out.println("      1     2     3     4     5     6     7     8     9     10");
 	for (int i = 0; i < mat.length; i++) {
 		System.out.println();
 		count++;
-		System.out.print(count + "  ");
+		System.out.print(Util.treateNumber(count) + "  ");
 	for (int j = 0; j < mat.length; j++) {
-		System.out.print(" [" + mat[i][j] + "]" + " ");
+		System.out.print(" [" + Util.treateNumber(mat[i][j]) + "]" + " ");
 	}
 	}
 	
---
+++ src/br/com/icts/utilidades/Util.java
@@ -0,0 +1,8 @@
+package br.com.icts.utilidades;
+
+public class Util {
+public static String treateNumber (int number){
+	return (number<10) ? "0" + number : String.valueOf(number);
+	}
+
+}
