4e8562a3f5ba32ff8184443e27232d9d4e335c99
Augusto Mendes <augustomendes.mendes9@gmail.com>
2020-02-21 15:06:22 -0300

    adicionadas as funções

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,28 +3,18 @@ package application;
 import br.com.icts.utilidades.Util;
 
 public class Program {
-public static void main(String[] args) {
-	System.out.println("                          Tabuada");
-	System.out.println();
-	int[][] mat = new int [10][10];
-	int count = 0;
-	
-	for (int i = 0; i < mat.length; i++) {
-		for (int j = 0; j < mat.length; j++) {
-			mat[i][j] = (i +1) * (j +1) ;
+	public static void main(String[] args) {
+		int[][] mat = new int[10][10];
+
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				mat[i][j] = (i + 1) * (j + 1);
+			}
 		}
+		Util.printMat(mat);
 	}
-	System.out.println("      01    02    03    04    05    06    07    08    09    10");
-	for (int i = 0; i < mat.length; i++) {
-		System.out.println();
-		count++;
-		System.out.print(Util.treateNumber(count) + "  ");
-	for (int j = 0; j < mat.length; j++) {
-		System.out.print(" [" + Util.treateNumber(mat[i][j]) + "]" + " ");
-	}
-	}
-	
-	
-	
-}
+
+	// sequencia de fibonacci
+	// mudar a tabuada para x elevado a y
+
 }
--- src/br/com/icts/utilidades/Util.java
+++ src/br/com/icts/utilidades/Util.java
@@ -1,8 +1,19 @@
 package br.com.icts.utilidades;
 
 public class Util {
-public static String treateNumber (int number){
-	return (number<10) ? "0" + number : String.valueOf(number);
+	public static String treateNumber(int number) {
+		return (number < 10) ? "0" + number : String.valueOf(number);
 	}
 
+	public static void printMat(int mat[][]) {
+		System.out.println("                          Tabuada\n");
+		System.out.println("      01    02    03    04    05    06    07    08    09    10\n");
+		for (int i = 0; i < mat.length; i++) { //linha
+			String line = Util.treateNumber(i + 1) + "  ";
+			for (int j = 0; j < mat.length; j++) { //coluna
+				line += " [" + Util.treateNumber(mat[i][j]) + "]" + " ";
+			}
+			System.out.println(line);
+		}
+	}
 }
